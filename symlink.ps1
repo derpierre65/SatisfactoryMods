66480b10943718c3f1f75d79e071f8cadb23bb02
derpierre65 <git@derpierre65.de>
2024-10-13 15:45:44 +0100

    feat(DoggoNotification): initial commit

---
+++ symlink.ps1
@@ -0,0 +1,29 @@
+param (
+    [string]$SingleFolder = ""
+)
+
+$SMLPath = "M:\GameDev\Satisfactory\SatisfactoryModLoader\Mods"
+$ModDir = "M:\GameDev\Satisfactory\SatisfactoryMods"
+
+function New-Sym-Link ($Link) {
+    Remove-Item ($SMLPath + "\" + $Link) -ErrorAction SilentlyContinue -Recurse
+    New-Item -Path ($SMLPath + "\" + $Link) -ItemType SymbolicLink -Value ($ModDir + "\" + $Link) -Force
+}
+
+if ($SingleFolder -ne "") {
+    $dir = Get-ChildItem $ModDir | Where-Object {$_.PSISContainer -and $_.Name -eq $SingleFolder}
+    if ($dir) {
+        Write-Output "Processing folder: $($dir.Name)"
+        New-Sym-Link $dir.Name
+    }
+    else {
+        Write-Output "Folder '$SingleFolder' not found."
+    }
+}
+else {
+    $dir = Get-ChildItem $ModDir | Where-Object {$_.PSISContainer}
+    foreach ($d in $dir) {
+        Write-Output "Processing folder: $($d.Name)"
+        New-Sym-Link $d.Name
+    }
+}
