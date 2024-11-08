e1e9d99783ac52e39ea07f06debd910fa8932a23
derpierre65 <git@derpierre65.de>
2024-11-08 12:32:45 +0000

    chore(hard-drive-rerolls): init commit

---
+++ MoreHardDriveRerolls/Source/MoreHardDriveRerolls/Private/MoreHardDriveRerolls.cpp
@@ -0,0 +1,49 @@
+﻿#include "MoreHardDriveRerolls.h"
+#include "FGResearchManager.h"
+#include "FGResearchSettings.h"
+#include "SessionSettingsManager.h"
+
+void AMoreHardDriveRerolls::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UpdateMaxRerolls();
+}
+
+AMoreHardDriveRerolls::AMoreHardDriveRerolls()
+{
+	ReplicationPolicy = ESubsystemReplicationPolicy::SpawnOnServer_Replicate;
+}
+
+void AMoreHardDriveRerolls::UpdateMaxRerolls()
+{
+	if (auto* ResearchManager = GetMutableDefault<UFGResearchSettings>())
+	{
+		const auto sessionManager = GetWorld()->GetSubsystem<USessionSettingsManager>();
+		auto MaxHardDriveRescans = static_cast<int>(sessionManager->GetFloatOptionValue("MaxHardDriveRescans"));
+
+		ResearchManager->mNumRerollsPerHardDrive = MaxHardDriveRescans > 0 ? MaxHardDriveRescans : 1;
+	}
+}
+
+FRerollHardDriveData AMoreHardDriveRerolls::GetHardDriveData(const UFGHardDrive* HardDrive)
+{
+	auto UnclaimedHardDriveData = AFGResearchManager::Get(GetWorld())->mUnclaimedHardDriveData;
+	FRerollHardDriveData HardDriveData;
+
+	for (const auto UnclaimedData : UnclaimedHardDriveData)
+	{
+		if (UnclaimedData.HardDrive != HardDrive) continue;
+
+		const auto NumRerollPerHardDrive = UFGResearchSettings::Get()->mNumRerollsPerHardDrive;
+		const int32 RerollsExecuted = UnclaimedData.PendingRewardsRerollsExecuted;
+		const int32 RemainingRerolls = NumRerollPerHardDrive - RerollsExecuted;
+
+		HardDriveData.PendingRewardsRerollsExecuted = RerollsExecuted;
+		HardDriveData.RemainingRerolls = RemainingRerolls > 0 ? RemainingRerolls : 0;
+
+		break;
+	}
+
+	return HardDriveData;
+}
---
+++ MoreHardDriveRerolls/Source/MoreHardDriveRerolls/Public/MoreHardDriveRerolls.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "FGResearchManager.h"
+#include "Subsystem/ModSubsystem.h"
+#include "MoreHardDriveRerolls.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRerollHardDriveData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 PendingRewardsRerollsExecuted = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RemainingRerolls = 0;
+};
+
+UCLASS()
+class MOREHARDDRIVEREROLLS_API AMoreHardDriveRerolls : public AModSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	AMoreHardDriveRerolls();
+	
+	UFUNCTION(BlueprintCallable, Category = "More Hard Drive Rerolls")
+	FRerollHardDriveData GetHardDriveData(const UFGHardDrive* HardDrive);
+
+	UFUNCTION(BlueprintCallable, Category = "More Hard Drive Rerolls")
+	void UpdateMaxRerolls();
+	
+protected:
+	virtual void BeginPlay() override;
+};
